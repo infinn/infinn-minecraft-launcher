2b243b5ceec5f332d59247324082b8e723b20bae
infinn <mathius.05.mq@gmail.com>
2025-09-30 15:19:11 -0300

    Add user ram

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,33 @@
 import json
+import os
 import minecraft_launcher_lib
 import subprocess
+import ctypes
+
+from src.config import VERSION_LAUNCHER
 
 class MineManager:
     def __init__(self, user):
         self.MINECRAFT_DIRECTORY = f"C://Users//{user}//AppData//Roaming//.minecraft"
         self.SRC_JSON = f"{self.MINECRAFT_DIRECTORY}//configuration.json"
 
+        self._ensure_file()
         
-        self.callback = {
-            "setStatus": "set_status",
-            "setProgress": "set_progress",
-            "setMax": "set_max"
+        self.mine.callback = {
+            "setStatus": self.set_status,
+            "setProgress": self.set_progress,
+            "setMax": self.set_max,
         }
-        
+    
+    def _ensure_file(self):
+        """
+        if not os.path.exists(self.MINECRAFT_DIRECTORY):
+            os.makedirs(self.MINECRAFT_DIRECTORY)
+        """
+        if not os.path.isfile(self.SRC_JSON):
+            with open(self.SRC_JSON, "w", encoding="utf-8") as f:
+                json.dump({}, f, indent=4)  # archivo JSON vacío
+
     def get_local_version(self):
         local_version = []
 
@@ -24,15 +38,51 @@ class MineManager:
 
         return local_version
     
-    def get_minecrat_directory(self):
-        return self.MINECRAFT_DIRECTORY
     
     def set_minecrat_directory(self, path):
         self.MINECRAFT_DIRECTORY = path
         self.SRC_JSON = f"{self.MINECRAFT_DIRECTORY}//configuration.json"
+
+    def get_user_ram(self):
+        class MEMORYSTATUSEX(ctypes.Structure):
+            _fields_ = [
+                ("dwLength", ctypes.c_ulong),
+                ("dwMemoryLoad", ctypes.c_ulong),
+                ("ullTotalPhys", ctypes.c_ulonglong),
+                ("ullAvailPhys", ctypes.c_ulonglong),
+                ("ullTotalPageFile", ctypes.c_ulonglong),
+                ("ullAvailPageFile", ctypes.c_ulonglong),
+                ("ullTotalVirtual", ctypes.c_ulonglong),
+                ("ullAvailVirtual", ctypes.c_ulonglong),
+                ("sullAvailExtendedVirtual", ctypes.c_ulonglong),
+            ]
+
+        stat = MEMORYSTATUSEX()
+        stat.dwLength = ctypes.sizeof(MEMORYSTATUSEX)
+
+        ctypes.windll.kernel32.GlobalMemoryStatusEx(ctypes.byref(stat))
+
+        total_gb = stat.ullTotalPhys / (1024**3)
+        disponible_gb = stat.ullAvailPhys / (1024**3)
+        usado_gb = total_gb - disponible_gb
+
+        return {
+            "total": round(total_gb, 2),
+            "disponible": round(disponible_gb, 2),
+            "usado": round(usado_gb, 2),
+            "porcentaje": stat.dwMemoryLoad
+        }
     
     async def install_minecraft(self, version):
-        minecraft_launcher_lib.install.install_minecraft_version(version, self.MINECRAFT_DIRECTORY, callback=self.callback)
+        minecraft_launcher_lib.install.install_minecraft_version(
+            version,
+            self.MINECRAFT_DIRECTORY,
+            callback={
+                "setStatus": self.callback["setStatus"],
+                "setProgress": self.callback["setProgress"],
+                "setMax": self.callback["setMax"],
+            }
+        )
 
     async def play_minecraft(self, config):
         with open(self.SRC_JSON, "r") as file:
@@ -54,7 +104,7 @@ class MineManager:
                 f"-Xmx{ram}G",
                 f"-Xms{ram}G",
                 ],  # The jvmArguments
-            "launcherVersion": "0.0.1",
+            "launcherVersion": VERSION_LAUNCHER,
         }
 
         minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(version, self.MINECRAFT_DIRECTORY, options)
