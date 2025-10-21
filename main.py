3a54c241057028381adc684c762c4a531dec5187
infinn <mathius.05.mq@gmail.com>
2025-10-21 13:48:06 -0300

    Change rezize

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 from src.components.main_window import MainWindow
-from src.config import WINDOW_TITLE
+from src.config import WINDOW_TITLE, BG_BLACK
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title(WINDOW_TITLE)
+
+    root.resizable(False, False)
+    root.configure(bg=BG_BLACK)
+    root.iconbitmap('./logo.ico') 
 
     app = MainWindow(root)
 
